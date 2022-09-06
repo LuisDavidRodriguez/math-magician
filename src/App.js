539d7a4b7dca6dac6fd61c64e19ffbbe33d7dfd4
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-09-06 15:11:00 -0500

    Change the functional component App for a Class based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
